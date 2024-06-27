333ec8c56c43c31e394852a1ce7e97694bd6550b
rubennavarrete <rd_navarrete@outlook.com>
2024-06-26 20:37:58 -0500

    update(enviroment fontend) dominio 26/6/2024

--- cliente/src/environments/environment.prod.ts
+++ cliente/src/environments/environment.prod.ts
@@ -1,9 +1,10 @@
 // Servidor de desarrollo
+// Servidor local
 const baseUrl = '';
 export const environment = {
   production: true,
   baseUrl: baseUrl,
-  url: '' + baseUrl,
-  urlLogOut: '' + baseUrl,
-  urlApi: '',
+  url: 'https://endo-solution.rubenvn.com/' + baseUrl,
+  urlLogOut: 'https://endo-solution.rubenvn.com/' + baseUrl,
+  urlApi: 'https://api-endo-solution.rubenvn.com',
 };
