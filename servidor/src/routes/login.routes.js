971accbbeae650459ade916a30fefbcf95e0ce9c
MelanySecaira <nayeli.secaira@espoch.edu.ec>
2024-12-04 05:17:01 -0500

    fix (login.router) eliminar forgotPassword 4/12/2024

--- servidor/src/routes/login.routes.js
+++ servidor/src/routes/login.routes.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { login, forgotPassword } from "../controllers/login.controller.js";
+import { login } from "../controllers/login.controller.js";
 
 const router = Router();
 
 
 router.post('/', login);
-router.post('/recuperar', forgotPassword);
+// router.post('/recuperar', forgotPassword);
 
 export default router;
